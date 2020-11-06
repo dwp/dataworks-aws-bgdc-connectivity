d366fd035a57e80f3e46e7691df6b5776c3a25a1
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-11-06 17:02:09 +0000

    format

--- connectivity.tf
+++ connectivity.tf
@@ -8,5 +8,5 @@ data "aws_vpc" "bgdc" {
 resource "aws_vpc_peering_connection" "bgdc_to_internal_compute" {
   peer_vpc_id = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.vpc.id
   vpc_id      = data.aws_vpc.bgdc.id
-  tags = local.common_tags
+  tags        = local.common_tags
 }
