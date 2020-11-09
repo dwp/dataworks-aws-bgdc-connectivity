5eb26fa94cbe477f7657b77146dbed03ebe98b2b
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-11-09 09:13:10 +0000

    Add name tag to vpc peering

--- connectivity.tf
+++ connectivity.tf
@@ -8,5 +8,10 @@ data "aws_vpc" "bgdc" {
 resource "aws_vpc_peering_connection" "bgdc_to_internal_compute" {
   peer_vpc_id = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.vpc.id
   vpc_id      = data.aws_vpc.bgdc.id
-  tags        = local.common_tags
+  tags = merge(
+  local.common_tags,
+  {
+    Name = "bgdc-to-internal-compute"
+  },
+  )
 }
