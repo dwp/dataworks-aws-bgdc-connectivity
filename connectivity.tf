386ae477fc0a176c4f626bf3e35bb45d4725c048
Dmitri Kovalenko <39998089+tea-short@users.noreply.github.com>
2020-11-09 13:25:02 +0000

    Update connectivity.tf
    Co-authored-by: Daniel Hill <dan@mamu.co>

--- connectivity.tf
+++ connectivity.tf
@@ -9,6 +9,7 @@ resource "aws_vpc_peering_connection" "bgdc_to_internal_compute" {
   peer_vpc_id = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.vpc.id
   vpc_id      = data.aws_vpc.bgdc.id
   auto_accept = true
+
   tags = merge(
     local.common_tags,
     {
