a73a8a66a13a200c1851d7a18a16428e0d981bdd
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-11-18 12:28:43 +0000

    Hive metastore is in PDM subnets, not ADG subnets

--- connectivity.tf
+++ connectivity.tf
@@ -26,19 +26,19 @@ data "aws_route_table" "bgdc_private" {
 }
 
 resource "aws_route" "bgdc_to_internal_compute" {
-  count                     = length(data.terraform_remote_state.internal_compute.outputs.adg_subnet.ids)
+  count                     = length(data.terraform_remote_state.internal_compute.outputs.pdm_subnet.ids)
   route_table_id            = data.aws_route_table.bgdc_private.id
-  destination_cidr_block    = data.terraform_remote_state.internal_compute.outputs.adg_subnet.cidr_blocks[count.index]
+  destination_cidr_block    = data.terraform_remote_state.internal_compute.outputs.pdm_subnet.cidr_blocks[count.index]
   vpc_peering_connection_id = aws_vpc_peering_connection.bgdc_to_internal_compute.id
 }
 
-data "aws_route_table" "adg" {
-  subnet_id = data.terraform_remote_state.internal_compute.outputs.adg_subnet.ids[0]
+data "aws_route_table" "pdm" {
+  subnet_id = data.terraform_remote_state.internal_compute.outputs.pdm_subnet.ids[0]
 }
 
-resource "aws_route" "adg_to_bgdc" {
+resource "aws_route" "pdm_to_bgdc" {
   count                     = length(tolist(data.aws_subnet_ids.bgdc_private.ids))
-  route_table_id            = data.aws_route_table.adg.id
+  route_table_id            = data.aws_route_table.pdm.id
   destination_cidr_block    = data.aws_subnet.bgdc_private[count.index].cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.bgdc_to_internal_compute.id
 }
